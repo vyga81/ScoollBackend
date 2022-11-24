dfe1e2f27ce85f11ca5fb6eba944eb5e2e54206c
Vygantas Kazakevicius <104910000+vyga81@users.noreply.github.com>
2022-11-24 10:33:14 +0200

    micro update

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ScoollController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,4 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
+Route::resource('/posts', ScoollController::class);
 
